7b89880b1dfd1327c389b20a8d2a92f7a2843c3f
Mark McIver <mark@Marks-MacBook-Pro.local>
2019-06-13 12:34:23 -0600

    auth lecture wlh6

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+require('dotenv/config')
+
+const AuthCtrl = require('./controllers/auth')
+
+const app = express()
+
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+console.log(SESSION_SECRET)
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  console.log('db is connected')
+  app.listen(SERVER_PORT, () => console.log('port on', SERVER_PORT))
+})
+
+app.use(express.json())
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 365
+  }
+}))
+
+app.post('/auth/register', AuthCtrl.register)
+app.post('/auth/login', AuthCtrl.login)
+app.get('/auth/logout', AuthCtrl.logout)
+app.get('/auth/currentUser', AuthCtrl.currentUser)
+
